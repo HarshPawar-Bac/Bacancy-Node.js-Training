54426b4ab4c1784cfdf9cb59666b625b58416037
Harsh-3378 <pawarharsh756@gmail.com>
2026-02-11 18:24:47 +0530

    add seeder

---
+++ sequelize/seeders/20260211125056-demo-users.js
@@ -0,0 +1,25 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.bulkInsert("users", [
+      {
+        name: "Harsh",
+        email: "harsh@example.com",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: "meet",
+        email: "meet@example.com",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.bulkDelete("users", null, {});
+  }
+};
